968e2890221790056ef8c25943947ad127973487
duongthuynt97 <nguyenthithuyduong02@admicro.vn>
2020-08-14 16:54:42 +0700

    update

--- src/modules/group/users/UserScreen.js
+++ src/modules/group/users/UserScreen.js
@@ -6,29 +6,15 @@ import {
   StyleSheet,
   TouchableOpacity,
   ScrollView,
-  Animated
 } from 'react-native'
 
 import { fonts, colors } from '../../../styles'
 import { Button } from '../../../components'
-import VegaScrollList from 'react-native-vega-scroll-list';
 
-const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
 export default function UsersGroupScreen(props) {
   const {usersGroup} = props
   return (
-    <AnimatedFlatList
-      scrollEventThrottle={16}
-      bounces={false}
-      data={usersGroup}
-      renderItem={(data) => {
-        let item = renderItem(data);
-        const { index } = data;
-        return <VegaScrollItem {...{ index, y, item, distanceBetweenItem }} />;
-      }}
-      {...{ onScroll }}
-      {...otherProps}
-    />
+    <View></View>
   );
 }
